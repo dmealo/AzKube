b9062e92ea82147c22280d15f013daf06d8ecb38
dmealo <david.mealo@valtech.com>
2024-02-11 00:47:26 -0500

    Refactor AKS cluster setup script to support default settings and selection of all clusters in menu Rename script to match more flexible functionality

--- scripts/Setup-Aks-Kubectl.ps1
+++ scripts/Setup-Aks-Kubectl.ps1
@@ -19,10 +19,15 @@ param (
     [switch]
     $SkipTestConnections,
 
-    # Simple mode to get kubectl credentials for all AKS clusters without asking for user input
+    # Simple mode to get kubectl credentials for all AKS clusters found on all subscriptions in logged in tenant without asking for user input using default proxy
     [Parameter()]
     [switch]
-    $SimpleMode
+    $SetupAllWithDefaults,
+
+    # Initially select all AKS clusters for getting kubectl credentials
+    [Parameter()]
+    [switch]
+    $SelectAll
 )
 
 # Create a class with ToString method to display AKS clusters as a simple menu
@@ -67,7 +72,7 @@ function Get-KubectlCredentialsForAksClusters($aksClusters) {
     # Ask user if proxy should be used for all clusters
     Write-Host
     $useProxyAll = "y"
-    if (!$SkipProxyAll) {
+    if (!$SkipProxyAll -and !$SetupAllWithDefaults) {
         $useProxyAll = ($answer = (Read-Host "Use proxy ($proxyUrl) for all clusters? (Y/n/none)").ToLower()) ? $answer : $useProxyAll
     }
 
@@ -121,26 +126,31 @@ if ((az extension list | ConvertFrom-Json | Where-Object { $_.name -eq 'resource
 # Get all AKS clusters into a variable using Azure Resource Graph
 $aksClusters = (az graph query -q "where type == 'microsoft.containerservice/managedclusters' | project name, subscriptionId, resourceGroup" | ConvertFrom-Json).data | ForEach-Object { [Cluster]::new($_.name, $_.subscriptionId, $_.resourceGroup) }
 
+if ($aksClusters.Count -eq 0) {
+    Write-Host "No AKS clusters found to get kubectl credentials for. Verify that you have logged into the correct Azure subscription(s) with permission to access AKS clusters and retry." -ForegroundColor Orange
+    exit 0
+}
+
 # Install PSMenu if not already installed
 if (-not (Get-Command Show-Menu -ErrorAction SilentlyContinue)) {
     Install-Module -Name PSMenu -Force
 }
 
-# Diaplay AKS clusters as a text list for display only
-if ($aksClusters.Count -eq 0) {
-    Write-Host "No AKS clusters found to get kubectl credentials for. Verify that you have logged into the correct Azure subscription(s) with permission to access AKS clusters and retry." -ForegroundColor Orange
-    exit 0
+if ($SetupAllWithDefaults) {
+    # Continue execution
+    Write-Host "Setting up all AKS clusters with default settings:" -ForegroundColor Cyan
+    Write-Host $($aksClusters | ForEach-Object { "`n" + $_.ToString() } ) -ForegroundColor Cyan
 }
-# elseif (!$SimpleMode) {
+else {
     Write-Host
     Write-Host "Select AKS cluster(s) to get kubectl credentials for:"
     # Display AKS clusters as a simple menu for selection
     $clusterIndexes = $($aksClusters | ForEach-Object { [int]$aksClusters.IndexOf($_) })
-    $selectedClusters = Show-Menu -MenuItems $aksClusters -MultiSelect -InitialSelection ($SimpleMode ? $clusterIndexes : @())
+    $selectedClusters = Show-Menu -MenuItems $aksClusters -MultiSelect -InitialSelection ($SelectAll ? $clusterIndexes : @())
     Write-Host 
     Write-Host "Selected AKS cluster(s): $($selectedClusters | ForEach-Object { "`n" + $_.ToString() } )" -ForegroundColor Cyan
     $aksClusters = $selectedClusters
-# }
+}
 
 # Get kubectl credentials for the selected AKS clusters
 Get-KubectlCredentialsForAksClusters $aksClusters
