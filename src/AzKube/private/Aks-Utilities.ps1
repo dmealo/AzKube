d4de34a06e12615821448a35491d330a4e433d3c
dmealo <david.mealo@valtech.com>
2025-02-17 00:49:43 -0500

    add artifact upload/download steps to allow repo script to run

--- src/AzKube/private/Aks-Utilities.ps1
+++ src/AzKube/private/Aks-Utilities.ps1
@@ -104,14 +104,12 @@ function Install-AzureCli {
 # Install Azure Powershell module if not already installed
 function Install-AzModule {
     if (-not (Get-Command Connect-AzAccount -ErrorAction SilentlyContinue)) {
-        Write-Host "Installing Az module..."
         Install-Module -Name Az -Force
     }
 }
 
 # Install PSMenu if not already installed
 function Install-PSMenu {
-    Write-Host "Installing PSMenu module..."
     if (-not (Get-Command Show-Menu -ErrorAction SilentlyContinue)) {
         Install-Module -Name PSMenu -Force
     }
@@ -119,7 +117,7 @@ function Install-PSMenu {
 
 # Function to install kubectl using WinGet if not already installed
 function Install-Kubectl {
-    Write-Host "Installing kubectl using WinGet..."
+    # Install kubectl using WinGet if not already installed
     if (-not (Get-Command kubectl -ErrorAction SilentlyContinue)) {
         winget install --id Kubernetes.kubectl -e
     }
